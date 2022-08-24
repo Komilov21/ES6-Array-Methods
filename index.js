f19908b4aa3bcc1e9adf144fd37d86b2b3223173
Komilov Khayrulla <108469321+Komilov21@users.noreply.github.com>
2022-08-25 00:22:36 +0200

    updated

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-window.onload = ()=>{
+
 const getImages = async () =>{
-    const response = await fetch("https://striveschool-api.herokuapp.com/books")
-    const books =await response.json()
-    console.log(books)
+    let books
+
+    try {
+        let res = await fetch("https://striveschool-api.herokuapp.com/books")
+        books = await res.json()
+
+        books.map(book => {
+            let container = document.querySelectorAll(".row")[0]
+            let newElem = document.createElement("div")
+            let img = document.createElement('img')
+            img.setAttribute('src', book.img)
+            // newElem.className = "col-md-4 "
+            newElem.appendChild(img)
+            
+    
+            // console.log(container)
+            container.appendChild(newElem)
+        }) 
+        
+
+    } catch (error) {
+        console.log(error)
+    };
+    // const container = document.querySelectorAll(".row")
+ 
 }
-}
+getImages()
+window.onload = ()=>{}
+
+// const response = await fetch("https://striveschool-api.herokuapp.com/books")
+// const books =await response.json()
+// console.log(books)
+// const container = document.getElementsByClassName("row")
+// const newElem =document.createElement("div")
+// newElem.classList.add("col")
+// books.map(book) => {
